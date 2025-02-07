78a910a456d0fd36092974f58a1e577a76f5c75c
Charis Grubb <charisgrubb@gmail.com>
2025-02-07 10:39:19 -0500

    More Uber Analysis

--- Uber/UberAnalysis.py
+++ Uber/UberAnalysis.py
@@ -57,7 +57,7 @@ ax_hist.axvline(df.pickups.mean(), color='g', linestyle='--')      # Add mean to
 ax_hist.axvline(df.pickups.median(), color='black', linestyle='-') # Add median to the histogram
 
 """Distribution of hourly pickups is highly right-skewed """
-plt.savefig("Uber\Plots\pickups_box_hist.png")
+plt.savefig("Uber\\Plots\\pickups_box_hist.png")
 
 f, (ax_box, ax_hist) = plt.subplots(
         nrows=2,  # Number of rows of the subplot grid
@@ -71,13 +71,62 @@ sns.histplot(x=df.vsb, kde=False, ax=ax_hist, bins="auto")
 ax_hist.axvline(df.vsb.mean(), color='g', linestyle='--')      # Add mean to the histogram
 ax_hist.axvline(df.vsb.median(), color='black', linestyle='-') # Add median to the histogram
 
-"""Distribution of hourly vsb (Visibility) is highly right-skewed """
-plt.savefig("Uber\Plots\\vsb_box_hist.png")
+"""Distribution of vsb (Visibility) is left-skewed """
+plt.savefig("Uber\\Plots\\vsb_box_hist.png")
 
-#What are the different variables that influence pickups?
+f, (ax_box, ax_hist) = plt.subplots(
+        nrows=2,  # Number of rows of the subplot grid
+        sharex=True,  # The X-axis will be shared among all the subplots
+        gridspec_kw={"height_ratios": (.25, .75)},
+        figsize=(15,10)
+    )
+
+sns.boxplot(x=df.sd, ax=ax_box, showmeans=True, color='red')
+sns.histplot(x=df.sd, kde=False, ax=ax_hist, bins="auto")
+ax_hist.axvline(df.sd.mean(), color='g', linestyle='--')      # Add mean to the histogram
+ax_hist.axvline(df.sd.median(), color='black', linestyle='-') # Add median to the histogram
+
+"""Distribution of sd (Snow Depth) """
+plt.savefig("Uber\Plots\\sd_box_hist.png")
+
+####MULTIVARIATE#############
+num_var = ['pickups', 'spd', 'vsb', 'temp', 'dewp', 'slp', 'pcp01', 'pcp06', 'pcp24', 'sd']
+corr = df[num_var].corr()
+
+# Plot the heatmap
+plt.figure(figsize = (14, 10))
+sns.heatmap(corr, annot = True, cmap = 'coolwarm',            
+        fmt = ".1f",            
+        xticklabels = corr.columns,            
+        yticklabels = corr.columns)
+"""Dew POint (an indication of humidity) appears highly coorelated with temperature.
+Visibility is negatively coorelated with precipitation - meaning, as precipitation increases, visibility goes down
+Snow depth is negatively coorelated with temperature, as expected.
+NOTE: Correlation does not imply causation."""
+plt.savefig("Uber\\Plots\\heatmap.png")
 
 
-#Which factor affects the pickups the most? What could be plausible reasons for that?
+#### # of PICKUPS GROUPED BY MONTH NAME
+df['start_month'] = df.pickup_dt.dt.month_name()
+cats = df.start_month.unique().tolist() #Get a list of all month start dates
+df.start_month = pd.Categorical(df.start_month, ordered=True, categories = cats)#Order data
+plt.figure(figsize=(20,7))
+sns.lineplot(x = 'start_month', y='pickups', data=df, errorbar=('ci', 0), color="PURPLE", estimator="sum")
+plt.ylabel("Total Pickups")
+plt.xlabel("Month")
+plt.savefig("Uber\\Plots\\PickupsByMonthName.png")
 
 
-#What are your recommendations to Uber management to capitalize on fluctuating demand?
+#### AVERAGE OF PICKUPS GROUPED BY WEEK DAY NAME
+df['week_day'] = df.pickup_dt.dt.day_name()
+cats = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday']
+df.week_day = pd.Categorical(df.week_day, ordered = True, categories = cats)
+plt.figure(figsize = (20, 7))
+sns.lineplot(x = "week_day", y = "pickups", errorbar=('ci', 0), data = df, color = "RED")
+plt.ylabel('AVG(Mean) pickups')
+plt.xlabel('DAY OF WEEK')
+"""SATURDAY has the most AVG pickups, with MONDAY being the least"""
+plt.savefig("Uber\\Plots\\AVG_pickups_by_weekday.png")
+
+
+
---
+++ Uber/UberConclusion.md
@@ -0,0 +1,15 @@
+# UBER PICKUPS CONCLUSION AND RECOMMENDATIONS
+
+
+#### What are the different variables that influence pickups?
+
+ - Day of the week influenced the number of pickups with Saturday being the day with the most, and Monday being the day with the least. 
+ ![AVG Pickups By Week Day](Plots\AVG_pickups_by_weekday.png)
+
+
+#### Which factor affects the pickups the most? What could be plausible reasons for that?
+
+
+##### What are your recommendations to Uber management to capitalize on fluctuating demand?
+
+-  #1 Suggest scheduling a heavier staff on the weekend, especially Saturday, to ensure cab availability. Also suggest slimming the staff down during the week, especially Monday. 
