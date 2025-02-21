4dadd402666608749d65506612f0fa4f45f7e8b6
Charis <charisgrubb@gmail.com>
2025-02-21 14:34:22 -0500

    fsc analysis plus plotter updates

--- Fantasy_Sports_Clustering/FSC_Analysis.py
+++ Fantasy_Sports_Clustering/FSC_Analysis.py
@@ -11,11 +11,32 @@ warnings.filterwarnings("ignore")
 
 
 def analyze():
+    with open('Fantasy_Sports_Clustering\\analysis_output.txt','w') as file:
+        data = pd.read_csv("Fantasy_Sports_Clustering\\fpl_data.csv")
+        data_copy = data.copy() #Copy in order to maintain original data for reference
+
+        #Log the dataset shape
+        rows,cols = data.shape
+        file.write(f"Rows/Columns: {rows}/{cols} \n")
 
 
-    data = pd.read_csv("Fantasy_Sports_Clustering\\fpl_data.csv")
-    data_copy = data.copy() #Copy in order to maintain original data for reference
+        dup_sum = data_copy.duplicated().sum() 
+        file.write(f"Duplicate summary: {dup_sum}\n")
 
-    dup_sum = data_copy.duplicated().sum() 
+        data_nulls = data_copy.isnull().sum()
 
-    print(dup_sum)
+        file.write(f"Number of NULLs per Column:\n {data_nulls} \n")
+
+        for i in data_nulls:
+            if i>0:
+                print("Add logic to fix null data-", data_nulls.index[i])
+                file.write("Fixing NULL data at " + data_nulls.index[i])
+
+        
+        #Univariate Analysis
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Goals_Scored,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_goalsscored.png")
+        
+        
+        
+        
+        file.close()
--- utility/plotter.py
+++ utility/plotter.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set_theme(style='darkgrid')
 
 class Plotter_Helper:
+
+    @classmethod
     def box_hist_plot(cls, x, save_path):
         
         f, ax_box, ax_hist = cls.sub_plots()
@@ -15,7 +18,7 @@ class Plotter_Helper:
         
         plt.savefig(save_path)
 
-
+    @classmethod
     def heatmapper(cls,coor_df, save_path, cmap='coolwarm', annot=True, figsize=(14,10), fmt=".1f"):
         
         plt.figure(figsize = figsize)
@@ -27,6 +30,7 @@ class Plotter_Helper:
 
         plt.savefig(save_path)
 
+    @classmethod
     def lineplot(cls,df, x,y,save_path, ci=0,color="PURPLE", estimator = "sum",figsize=(20,7), x_label = None, y_label = None):
 
         plt.figure(figsize = figsize)
@@ -35,7 +39,8 @@ class Plotter_Helper:
         plt.ylabel(y if y_label is None else y_label)
         plt.xlabel(x if x_label is None else x_label)
         plt.savefig(save_path)
-
+    
+    @classmethod
     def sub_plots(cls, nrows = 2,  gridspec_kw = {"height_ratios": (.25, .75)}
                   , figsize = (15,10)):
         
