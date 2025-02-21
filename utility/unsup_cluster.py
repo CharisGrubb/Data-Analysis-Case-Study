68d57798fb656690067c4291448dbe489a1a70ab
Charis <charisgrubb@gmail.com>
2025-02-21 14:47:08 -0500

    starting cluster helper file

---
+++ utility/unsup_cluster.py
@@ -0,0 +1,12 @@
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import StandardScaler
+
+
+class Cluster_Helper:
+
+    @classmethod 
+    def scaler(cls):
+        pass
