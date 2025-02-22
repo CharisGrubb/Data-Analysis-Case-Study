d1673146911e3514ed7696ed32f97e5110cfda9c
Charis <charisgrubb@gmail.com>
2025-02-22 11:52:19 -0500

    univariate analysis for fsc

--- Fantasy_Sports_Clustering/FSC_Analysis.py
+++ Fantasy_Sports_Clustering/FSC_Analysis.py
@@ -1,9 +1,5 @@
-from scipy.spatial.distance import cdist, pdist
-from sklearn.cluster import DBSCAN, KMeans
-from sklearn.metrics import silhouette_score
-from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import StandardScaler
-from utility import plotter
+
+from utility import plotter, unsup_cluster
 import pandas as pd
 import warnings
 
@@ -35,6 +31,15 @@ def analyze():
         
         #Univariate Analysis
         plotter.Plotter_Helper.box_hist_plot(x= data_copy.Goals_Scored,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_goalsscored.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Assists,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_assists.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Goals_Conceded,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_goalsconceded.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Clean_Sheets,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_cleansheets.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Minutes,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_mins.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Total_Points,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_totalpnts.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Creativity,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_creativity.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Influence,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_influence.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Threat,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_threat.png")
+        plotter.Plotter_Helper.box_hist_plot(x= data_copy.Bonus,save_path="Fantasy_Sports_Clustering\\Plots\\histoboxplot_bonus.png")
         
         
         
